8d77a23860c0f68546e3da9ea2654ba12f8e5280
YOGEESH K P <yogeesh793@gmail.com>
2025-02-04 10:44:01 +0530

    Add files via upload

---
+++ 2.numpy.py
@@ -0,0 +1,14 @@
+#Create a 1-D array containing the values 1,2,3,4,5:
+import numpy
+arr = numpy.array([1, 2, 3, 4, 5])
+print(arr)
+
+# Create a 2-D array containing two arrays with the values 1,2,3 and 4,5,6:
+import numpy
+ay = numpy.array([[1, 2, 3], [4, 5, 6]])
+print(ay)
+
+# 3
+import numpy
+py = numpy.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+print(py)
